5c119b1b16f3addbf738575fd00cb168ffd28e0c
Venaseph <cperagine@gmail.com>
2019-03-26 14:19:19 -0400

    removed regex in favor of isalnum()

--- nines.py
+++ nines.py
@@ -7,18 +7,16 @@ import argparse
 
 def main():
     args = argparser()
-    # print(args.match)
     cwd = getcwd()
     print(args.match)
     print(args.replace)
- 
-
     return 0
 
 
 # Arge Parser Setup
 def argparser():
-    parser = argparse.ArgumentParser(description='Finds and replaces all instances of a User Id passed as an positional argument in Dolphin data folders, file names, as well as .txts recursively. If using special characters, please enclose match and replace arguments in quotes.')
+    parser = argparse.ArgumentParser(description='Finds and replaces all instances of a User Id passed as an positional argument in Dolphin data folders, '
+    'file names, as well as .txts recursively. If using special characters, please enclose match and replace arguments in quotes.')
     # Parser matcha dn replace arguments, required fields
     parser.add_argument('match', help='UserId to match and replace (Required)')
     parser.add_argument('replace', help='Replacement string for match values')
@@ -27,18 +25,11 @@ def argparser():
 
     # Check validity of arguments before returning
     for key, value in vars(args).items():
-        if not checkArgsValidity(value):
+        if not value.isalnum():
             print(value + " is an invalid " + key + " argument")
             sys.exit()
     return args
 
-
-def checkArgsValidity(argument):
-    # Regex match Letters/Number ONLY on String
-    if re.match('[A-Za-z0-9]+', argument):
-        return True
-    else:
-        return False
 
 # Gets current workding dir 
 def getcwd():
