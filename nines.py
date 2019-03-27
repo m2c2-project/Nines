a737d6469517d38446c5782c6a5dc650690d1e48
Venaseph <cperagine@gmail.com>
2019-03-27 15:26:25 -0400

    Cleaned up test folder, replaceString functional on file level

--- nines.py
+++ nines.py
@@ -8,9 +8,17 @@ import argparse
 ROOT_DIRECTORY = os.getcwd()
 TEXTEXT = '.txt'
 
+# Global Variables
+match = ""
+replace = ""
+
+
 def main():
+    global match, replace
     # Collect args from input
     args = argParser()
+    match = args.match
+    replace = args.replace
     # Check to see if a match folder currently exists
     oldFolder = matchFolderExists(args.match)
     # Create replace folder
@@ -22,16 +30,41 @@ def main():
 
     return 0
 
+
 def iterateFilesInDir(directory):
     for file in os.listdir(directory):
         fileName = os.fsdecode(file)
         # if a text file and not a beep_log
         if fileName.endswith(TEXTEXT) and 'beep_log' not in fileName:
-            print(fileName)
+            fileUrl = os.path.join(directory, fileName)
+            data = openFile(fileUrl)
+            scrubbedData = replaceMatchStrings(data)
+            print(scrubbedData)
+
+            
+# Replaces instances of replace strings in a data file -> Scrubbed data
+def replaceMatchStrings(data):
+    idMatch = 'ID:' + match
+    idReplace = 'ID:' + replace
+    userIdMatch = 'user_id:' + match
+    userIdReplace = 'user_id:' + replace
+    userIDMatch = 'user_ID:' + match
+    userIDReplace = 'user_ID:' + replace
+    
+    for r in ((idMatch, idReplace), (userIdMatch, userIdReplace), (userIDMatch, userIDReplace)):
+        cleaned = data.replace(*r)
+        # cleaned = data.replace(idMatch, idReplace)
+    return cleaned
+
+
+# Opens file and reads data, closes file -> data
+def openFile(filePath):
+    with open(filePath, 'r') as file:
+        data = file.read()
+    return data
     
 
-
-# Check if match folder exists
+# Checks if match folder exists -> directory path
 def matchFolderExists(folderName):
     # Create path for folder in correct formatting for OS
     dirPath = os.path.join(getCwd(), folderName)
@@ -43,7 +76,7 @@ def matchFolderExists(folderName):
     return dirPath
 
 
-# Create new folder
+# Create new folder -> folder path
 def makeFolder(folderName):
     # Create path for folder in correct formatting for OS
     dirPath = os.path.join(getCwd(), folderName)
@@ -63,14 +96,14 @@ def makeFolder(folderName):
     return dirPath
 
 
-# Gets current workding dir 
+# Gets current workding dir  -> cwd
 def getCwd():
     # get current working dir
     cwd = os.getcwd()
     return cwd
 
 
-# Arge Parser Setup
+# Arge Parser Setup -> args object
 def argParser():
     parser = argparse.ArgumentParser(description='Finds and replaces all instances of a User Id passed as an positional argument in Dolphin data folders, '
     'file names, as well as .txts recursively. Will only work with Alphanumeric Characters')
