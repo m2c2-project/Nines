29d4f66a6b6e7e4bfcf19c9d1f0739f078009284
Venaseph <cperagine@gmail.com>
2019-03-27 13:25:27 -0400

    Added file iteration functionality and made adjustments to folder checks and creations

--- nines.py
+++ nines.py
@@ -4,51 +4,73 @@ import sys
 import os
 import argparse
 
+# Global Static Variables:
+ROOT_DIRECTORY = os.getcwd()
+TEXTEXT = '.txt'
+
 def main():
+    # Collect args from input
     args = argParser()
-    oldFolder = folderExists(args.replace)
+    # Check to see if a match folder currently exists
+    oldFolder = matchFolderExists(args.match)
+    # Create replace folder
     newFolder = makeFolder(args.replace)
+    # Switch to replace folder Dir
+    os.chdir(newFolder)
+    # iterate over all files in current dir
+    iterateFilesInDir(oldFolder)
 
     return 0
 
-
-# Check if replace folder exists
-def folderExists(folderName):
-    folderPath = os.path.join(getCwd(), folderName)
-    if not os.path.isdir(folderPath):
-        print("Target folder %s does not exist" % folderName)
-        sys.exit()
+def iterateFilesInDir(directory):
+    for file in os.listdir(directory):
+        fileName = os.fsdecode(file)
+        # if a text file and not a beep_log
+        if fileName.endswith(TEXTEXT) and 'beep_log' not in fileName:
+            print(fileName)
     
-    return folderPath
+
+
+# Check if match folder exists
+def matchFolderExists(folderName):
+    # Create path for folder in correct formatting for OS
+    dirPath = os.path.join(getCwd(), folderName)
+    # If folder doesn't exist, report and exit
+    if not os.path.isdir(dirPath):
+        print('Target match folder %s does not exist' % folderName)
+        sys.exit()
+    # Return folder path otherwise
+    return dirPath
 
 
 # Create new folder
 def makeFolder(folderName):
     # Create path for folder in correct formatting for OS
-    folderPath = os.path.join(getCwd(), folderName)
+    dirPath = os.path.join(getCwd(), folderName)
     # If folder doesn't exist, make it and report
-    if not os.path.isdir(folderPath):
+    if not os.path.isdir(dirPath):
         try:
-            os.mkdir(folderPath)
+            os.mkdir(dirPath)
         except OSError:
-            print("Creation of the replacement directory failed")
+            print('Creation of the replacement directory failed')
             sys.exit()
         else:
-            print("Successfully created the directory")
+            print('Successfully created the directory')
     else:
-        print("Cannot created directory, folder exists")
+        print('Cannot create replacement directory, folder already exists')
         sys.exit()
+    # Return folder path otherwise
+    return dirPath
 
-    return folderPath
 
 # Gets current workding dir 
 def getCwd():
     # get current working dir
     cwd = os.getcwd()
-    print("The current working directory is %s" % cwd)
     return cwd
 
-    # Arge Parser Setup
+
+# Arge Parser Setup
 def argParser():
     parser = argparse.ArgumentParser(description='Finds and replaces all instances of a User Id passed as an positional argument in Dolphin data folders, '
     'file names, as well as .txts recursively. Will only work with Alphanumeric Characters')
@@ -62,7 +84,7 @@ def argParser():
     # Check validity of arguments before returning
     for key, value in vars(args).items():
         if not value.isalnum():
-            print(value + " is an invalid " + key + " argument.")
+            print(value + ' is an invalid %s argument.' % key)
             sys.exit()
     return args
 
