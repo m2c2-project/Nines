b1e7b7368bc058823dfdd93d6cfddee95e56b39f
Venaseph <cperagine@gmail.com>
2019-03-27 10:26:23 -0400

    Added check function on dirs

--- nines.py
+++ nines.py
@@ -5,14 +5,51 @@ import os
 import argparse
 
 def main():
-    args = argparser()
-    folder = makeFolder(args.replace)
+    args = argParser()
+    oldFolder = folderExists(args.replace)
+    newFolder = makeFolder(args.replace)
 
     return 0
 
 
-# Arge Parser Setup
-def argparser():
+# Check if replace folder exists
+def folderExists(folderName):
+    folderPath = os.path.join(getCwd(), folderName)
+    if not os.path.isdir(folderPath):
+        print("Target folder %s does not exist" % folderName)
+        sys.exit()
+    
+    return folderPath
+
+
+# Create new folder
+def makeFolder(folderName):
+    # Create path for folder in correct formatting for OS
+    folderPath = os.path.join(getCwd(), folderName)
+    # If folder doesn't exist, make it and report
+    if not os.path.isdir(folderPath):
+        try:
+            os.mkdir(folderPath)
+        except OSError:
+            print("Creation of the replacement directory failed")
+            sys.exit()
+        else:
+            print("Successfully created the directory")
+    else:
+        print("Cannot created directory, folder exists")
+        sys.exit()
+
+    return folderPath
+
+# Gets current workding dir 
+def getCwd():
+    # get current working dir
+    cwd = os.getcwd()
+    print("The current working directory is %s" % cwd)
+    return cwd
+
+    # Arge Parser Setup
+def argParser():
     parser = argparse.ArgumentParser(description='Finds and replaces all instances of a User Id passed as an positional argument in Dolphin data folders, '
     'file names, as well as .txts recursively. Will only work with Alphanumeric Characters')
     
@@ -28,32 +65,6 @@ def argparser():
             print(value + " is an invalid " + key + " argument.")
             sys.exit()
     return args
-
-
-# Create new folder
-def makeFolder(folderName):
-    # Create path for folder in correct formatting for OS
-    dirFolder = os.path.join(getcwd(), folderName)
-    # If folder doesn't exist, make it and report
-    if not os.path.isdir(dirFolder):
-        try:
-            os.mkdir(dirFolder)
-        except OSError:
-            print("Creation of the replacement directory failed")
-        else:
-            print("Successfully created the directory")
-    else:
-        print("Folder exists")
-
-    return dirFolder
-
-
-# Gets current workding dir 
-def getcwd():
-    # get current working dir
-    cwd = os.getcwd()
-    print("The current working directory is %s" % cwd)
-    return cwd
 
 
 # Maincontrol
